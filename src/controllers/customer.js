5dfb9e617632feecb4870cdcb4514af8a1bab94c
Pathum Kalhan <pathum@earltech.biz>
2020-08-25 10:02:50 +0530

    customer

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -64,4 +64,27 @@ router.get("/list", checkAuth, async (req, res) => {
   }
 });
 
+router.get("/drivers", checkAuth, async (req, res) => {
+  try {
+    const data = await db.user.findAll({
+      attributes: ["firstName", "lastName", "fullName", "id"],
+      order: db.sequelize.literal("id DESC"),
+      where: {
+        role: "driver",
+      },
+      raw: true,
+    });
+
+    data.forEach((element) => {
+      element.fullName = element.firstName + " " + element.lastName;
+    });
+
+   
+
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+});
+
 module.exports = router;
