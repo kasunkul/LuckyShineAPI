d22e04864aa237bf2d6b6e6c835641dd3e9cd12a
Pathum Kalhan <pathum@earltech.biz>
2020-08-26 10:54:20 +0530

    all missing cols to laundry orders

--- models/laundry_order.js
+++ models/laundry_order.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, Sequelize) => {
   class laundry_order extends Model {
     /**
@@ -10,54 +8,76 @@ module.exports = (sequelize, Sequelize) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      laundry_order.belongsTo(models.user,{foreignKey:'customerId',as:'customer'}),
-      laundry_order.belongsTo(models.user,{foreignKey:'customerId',as:'driver'})
+      laundry_order.belongsTo(models.user, {
+        foreignKey: "customerId",
+        as: "customer",
+      }),
+        laundry_order.belongsTo(models.user, {
+          foreignKey: "customerId",
+          as: "driver",
+        });
     }
-  };
-  laundry_order.init({
-    customerId: {
-      type: Sequelize.INTEGER,
-      references: {
-        model: "users",
-        key: "id",
+  }
+  laundry_order.init(
+    {
+      customerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "cascade",
+        onDelete: "cascade",
       },
-      onUpdate: "cascade",
-      onDelete: "cascade",
+      orderValue: {
+        type: Sequelize.DOUBLE(11, 2),
+      },
+      tax: {
+        type: Sequelize.DOUBLE(11, 2),
+      },
+      totalOrderAmount: {
+        type: Sequelize.DOUBLE(11, 2),
+      },
+      totalItems: {
+        type: Sequelize.INTEGER,
+      },
+      orderType: {
+        type: Sequelize.STRING,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      driverId: {
+        type: Sequelize.INTEGER,
+      },
+      assignDate: {
+        type: Sequelize.DATE,
+      },
+      startLocation: {
+        type: Sequelize.STRING,
+      },
+      notes: {
+        type: Sequelize.STRING,
+      },
+      orderPayed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      toPrint: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      deliveryDate: {
+        type: Sequelize.DATE,
+      },
+      shopId: {
+        type: Sequelize.INTEGER,
+      },
     },
-    orderValue: {
-      type: Sequelize.DOUBLE(11,2)
-    },
-    tax: {
-      type: Sequelize.DOUBLE(11,2)
-    },
-    totalOrderAmount: {
-      type: Sequelize.DOUBLE(11,2)
-    },
-    totalItems: {
-      type: Sequelize.INTEGER
-    },
-    orderType: {
-      type: Sequelize.STRING
-    },
-    status:{
-      type:Sequelize.STRING
-    },
-    driverId:{
-      type: Sequelize.INTEGER,
-    },
-    assignDate: {
-      type: Sequelize.DATE,
-    },
-    startLocation:{
-      type: Sequelize.STRING,
-    },
-    notes:{
-      type: Sequelize.STRING,
+    {
+      sequelize,
+      modelName: "laundry_order",
     }
-
-  }, {
-    sequelize,
-    modelName: 'laundry_order',
-  });
+  );
   return laundry_order;
 };
