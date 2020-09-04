f3baf4971312c810d411f345b9e933e57c7189ba
DeAlwis <dealwis@earltech.biz>
2020-09-04 14:29:53 +0530

    Change

---
+++ migrations/20200801005814-create-cart-item.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cart_items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      itemId: {
+        type: Sequelize.INTEGER
+      },
+      unitPrice: {
+        type: Sequelize.DOUBLE(11,2)
+      },
+      units: {
+        type: Sequelize.INTEGER
+      },
+      needIron: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('cart_items');
+  }
+};
