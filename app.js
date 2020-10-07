fd3b38c1a2be05351b4054dcfc94466c27cee5f8
Pathum Kalhan <pathum@earltech.biz>
2020-10-07 21:36:04 +0530

    swagger comment

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const morgan = require('morgan');
 
 app.use(morgan('dev'));
 const bodyParser = require('body-parser');
-const { swaggerDocument } = require('./swagger.js');
+// const { swaggerDocument } = require('./swagger.js');
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
