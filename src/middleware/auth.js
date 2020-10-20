4251e431f9c52d8f5ee79a80a9d1a802c94b7893
Kasun Kulathunga <dealwis@earltech.biz>
2020-10-20 22:05:28 +0530

    Changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,19 +4,27 @@ const db = require('../../models');
 module.exports = async(req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(token, 'lavup');
 
-        console.log("decoded -- ", decoded);
+        if (token == "GUEST") {
+            req.user = { id: 0, iat: 1603211581 };
+            next();
+        } else {
+            const decoded = jwt.verify(token, 'lavup');
 
-        req.user = decoded;
-        // const { id } = req.user;
+            console.log("decoded -- ", decoded);
 
-        db.user.update({ lastSignOff: new Date() }, {
-            where: {
-                id: decoded.id,
-            },
-        }, );
-        next();
+            req.user = decoded;
+            // const { id } = req.user;
+
+            db.user.update({ lastSignOff: new Date() }, {
+                where: {
+                    id: decoded.id,
+                },
+            }, );
+            next();
+        }
+
+
     } catch (error) {
         res.sendStatus(401);
     }
